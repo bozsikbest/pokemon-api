dbd8b138077f8d1452d45172499affdb170a10ff
asd <asd>
2024-12-02 11:51:51 +0100

    asd

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-console.log("hello")
+//console.log("hello")
+var count =1
 
-function asd(text){
+function asd(text, src){
+    const img = document.createElement("img")
+    img.src=src
     const asd=document.createElement("div")
+    
     asd.textContent = text
+    asd.append(document.createElement("br"))
+    asd.append(img)
     document.body.appendChild(asd)
+    document.body.appendChild(document.createElement("br"))
+    
 }
 async function get(url) {
     const response = await fetch(url)
-    console.log(response)
-    const dataasd = await response.json()
-    console.log(dataasd.results)
-    for (let index = 0; index < dataasd.results.length; index++) {
-       asd(dataasd.results[index].name)
-       
+    //console.log(response)
+    const Data = await response.json()
+    
+    console.log(Data.results)
+    for (let index = 0; index < Data.results.length; index++) {
+        const poke_response = await fetch(Data.results[index].url)
+        //console.log(response)
+        const poke_Data = await poke_response.json()
+        console.log(poke_Data.sprites.front_default)
+        asd(Data.results[index].name, poke_Data.sprites.front_default)
+        count++
         
     }
-    if(dataasd.next != null) get(dataasd.next)
+    //if(Data.next != null) get(Data.next)
     
     
 }
